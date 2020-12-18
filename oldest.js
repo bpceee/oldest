9b002d5486d6331dd9991510a6eeaf4e2a22c2a3
Zijun Pan <swanf@outlook.com>
2020-12-18 22:10:33 +0800

    Update oldest.js

--- oldest.js
+++ oldest.js
@@ -3,10 +3,10 @@
     .then(res => res.text())
     .then(res => {
       let mainDocument = new DOMParser().parseFromString(res, 'text/html');
-      let commitCount = mainDocument.evaluate('//li[@class="commits"]//span', mainDocument.body).iterateNext().innerText;
+      let commitCount = mainDocument.evaluate('//span[@class="d-none d-sm-inline"]//strong', mainDocument.body).iterateNext().innerText;
       commitCount = Number(commitCount.trim().replace(',', ''));
-      let commitId = mainDocument.evaluate('//*[@class="commit-tease commit-loader"]', mainDocument.body).iterateNext().getAttribute("src").split('/').pop();
+      let commitId = mainDocument.evaluate('//*[@class="f6 link-gray text-mono ml-2 d-none d-lg-inline"]', mainDocument.body).iterateNext().getAttribute("href").split('/').pop();
       let url = `https://github.com/${repo}/commits/${branch}?after=${commitId}+${commitCount-10}`;
       window.location = url;
-    })
+  })
 })(window.location.pathname.match(/\/([^\/]+\/[^\/]+)(?:\/(?:tree|commits)\/(.+))?/));
